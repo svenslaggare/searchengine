ae8962e8bdd095c7bd2dc1dcd45877366444d8e9
Anton Jansson <anton_jansson@live.se>
2020-04-30 14:58:47 +0200

    Made crawler more general. It now sends it output to a channel. This made it possible to do crawl + index at the same time.

--- src/crawler.rs
+++ src/crawler.rs
@@ -64,64 +64,86 @@ impl CrawledLinks {
     }
 }
 
+#[derive(Debug)]
+pub struct CrawlResult {
+    pub url: Url,
+    pub content: String
+}
+
+pub fn get_save_path(parsed_url: &Url) -> (String, String) {
+    let save_path = parsed_url.path().to_owned() + &parsed_url.query().map(|x| format!("?{}", x)).unwrap_or(String::new());
+    let save_path_parts = save_path.split('/').filter(|x| x != &"").collect::<Vec<_>>();
+    let save_path_name: String = (*save_path_parts.last().unwrap()).to_owned();
+    let save_path_folder = save_path_parts[..save_path_parts.len() - 1].join("/");
+    println!("{}: {}-{}", save_path, save_path_folder, save_path_name);
+    (save_path_folder, save_path_name)
+}
 
 struct CrawlerWorker {
     link_extractor: LinkExtractor,
     crawled_links: Arc<RwLock<CrawledLinks>>,
     crawl_queue: Arc<Mutex<VecDeque<String>>>,
-    num_active_crawlers: Arc<AtomicU64>
+    results_sender: crossbeam::Sender<Option<CrawlResult>>,
+    num_active_requests: Arc<AtomicU64>,
+    num_crawled: Arc<AtomicU64>,
 }
 
 impl CrawlerWorker {
     fn new(crawled_links: Arc<RwLock<CrawledLinks>>,
-           crawl_queue: Arc<Mutex<VecDeque<String>>>) -> CrawlerWorker {
+           crawl_queue: Arc<Mutex<VecDeque<String>>>,
+           results_sender: crossbeam::Sender<Option<CrawlResult>>) -> CrawlerWorker {
         CrawlerWorker {
             link_extractor: LinkExtractor::new(),
             crawled_links,
             crawl_queue,
-            num_active_crawlers: Arc::new(AtomicU64::new(0))
+            results_sender,
+            num_active_requests: Arc::new(AtomicU64::new(0)),
+            num_crawled: Arc::new(AtomicU64::new(0))
         }
-    }
-
-    fn get_save_path(parsed_url: &Url) -> (String, String) {
-        let save_path = parsed_url.path().to_owned() + &parsed_url.query().map(|x| format!("?{}", x)).unwrap_or(String::new());
-        let save_path_parts = save_path.split('/').filter(|x| x != &"").collect::<Vec<_>>();
-        let save_path_name: String = (*save_path_parts.last().unwrap()).to_owned();
-        let save_path_folder = save_path_parts[..save_path_parts.len() - 1].join("/");
-        println!("{}: {}-{}", save_path, save_path_folder, save_path_name);
-        (save_path_folder, save_path_name)
     }
 
     async fn crawl(&self, url: String) {
         if let Ok(response) = reqwest::get(&url).await {
             let content = response.text().await.unwrap();
-            self.num_active_crawlers.fetch_sub(1, Ordering::SeqCst);
-
-            // Mark the link as crawled
-            self.crawled_links.write().unwrap().insert(url.to_owned());
+            self.num_active_requests.fetch_sub(1, Ordering::SeqCst);
 
             // Extract link
             let parsed_url = Url::parse(&url).unwrap();
             let base_url = format!("{}://{}", parsed_url.scheme(), parsed_url.domain().unwrap());
             let extracted_links = self.link_extractor.extract(&base_url, &content);
 
-            // Save content to disk
-            let (save_path_folder, save_path_name) = CrawlerWorker::get_save_path(&parsed_url);
-            tokio::task::spawn(async move {
-                let base_path = format!("data/{}", save_path_folder);
-                tokio::fs::create_dir_all(&base_path).await.unwrap();
-                tokio::fs::write(format!("{}/{}", base_path, save_path_name), content).await;
-            });
+            self.num_crawled.fetch_add(1, Ordering::SeqCst);
+
+            if parsed_url.path().starts_with("/wiki") {
+                self.results_sender.send(Some(CrawlResult {
+                    url: parsed_url,
+                    content
+                })).unwrap();
+
+                // let (save_path_folder, save_path_name) = get_save_path(&parsed_url);
+                // tokio::task::spawn(async move {
+                //     let base_path = format!("data/{}", save_path_folder);
+                //     tokio::fs::create_dir_all(&base_path).await.unwrap();
+                //     tokio::fs::write(format!("{}/{}", base_path, save_path_name), content).await;
+                // });
+            }
 
             // Insert the links
             {
-                let crawled_links_guard = self.crawled_links.read().unwrap();
+                let mut crawled_links_guard = self.crawled_links.write().unwrap();
                 let mut crawl_queue_guard = self.crawl_queue.lock().unwrap();
 
                 let mut total_links = 0;
                 let mut new_links = 0;
-                for link in extracted_links{
+                for link in extracted_links {
+                    let link_url = Url::parse(&link).unwrap();
+                    let normalized_link = link_url.scheme().to_owned() + "://" + link_url.domain().unwrap() + link_url.path();
+
+                    // println!("{} -> {}", link, normalized_link);
+                    let link = normalized_link;
+
                     if !crawled_links_guard.contains(&link) {
+                        crawled_links_guard.insert(link.clone());
                         crawl_queue_guard.push_back(link);
                         new_links += 1;
                     }
@@ -129,10 +151,10 @@ impl CrawlerWorker {
                     total_links += 1;
                 }
 
-                println!("{}: {}/{}", url, new_links, total_links);
+                println!("Result for {}: {}/{}", url, new_links, total_links);
             }
         } else {
-            self.num_active_crawlers.fetch_sub(1, Ordering::SeqCst);
+            self.num_active_requests.fetch_sub(1, Ordering::SeqCst);
             println!("Failed to crawl url: {}", url);
         }
     }
@@ -141,45 +163,58 @@ impl CrawlerWorker {
 pub struct Crawler {
     crawled_links: Arc<RwLock<CrawledLinks>>,
     crawl_queue: Arc<Mutex<VecDeque<String>>>,
-    worker: Arc<CrawlerWorker>
+    pub result_receiver: crossbeam::Receiver<Option<CrawlResult>>,
+    worker: Arc<CrawlerWorker>,
 }
 
 impl Crawler {
     pub fn new() -> Crawler {
         let crawled_links = Arc::new(RwLock::new(CrawledLinks::new()));
         let crawl_queue = Arc::new(Mutex::new(VecDeque::new()));
+        let (result_sender, result_receiver) = crossbeam::unbounded();
 
         Crawler {
             crawled_links: crawled_links.clone(),
             crawl_queue: crawl_queue.clone(),
-            worker: Arc::new(CrawlerWorker::new(crawled_links, crawl_queue))
+            result_receiver,
+            worker: Arc::new(CrawlerWorker::new(crawled_links, crawl_queue, result_sender)),
         }
     }
 
+
     pub fn run(&self, start_url: &str) {
         self.crawl_queue.lock().unwrap().push_back(start_url.to_owned());
+        let start_time = std::time::Instant::now();
 
         loop {
             println!(
                 "Num active: {}, total crawled: {}",
-                self.worker.num_active_crawlers.load(Ordering::SeqCst),
-                self.crawled_links.read().unwrap().len()
+                self.worker.num_active_requests.load(Ordering::SeqCst),
+                self.worker.num_crawled.load(Ordering::SeqCst)
             );
 
-            if self.worker.num_active_crawlers.load(Ordering::SeqCst) < 100 {
+            if self.worker.num_active_requests.load(Ordering::SeqCst) < 100 {
                 if let Some(current_url) = self.crawl_queue.lock().unwrap().pop_front() {
                     println!("Crawling url: {}", current_url);
-                    self.worker.num_active_crawlers.fetch_add(1, Ordering::SeqCst);
+                    self.worker.num_active_requests.fetch_add(1, Ordering::SeqCst);
 
                     let worker_clone = self.worker.clone();
                     tokio::task::spawn(async move {
                         worker_clone.crawl(current_url).await;
                     });
                 }
+
+                if self.worker.num_active_requests.load(Ordering::SeqCst) == 0 {
+                    std::thread::sleep(std::time::Duration::from_millis(10));
+                }
             } else {
                 std::thread::sleep(std::time::Duration::from_millis(10));
+            }
+
+            if (std::time::Instant::now() - start_time).as_secs() >= 1200 {
+                self.worker.results_sender.send(None);
+                break;
             }
         }
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::path::{Path, PathBuf};
 use std::fs::{DirEntry, File};
-use std::sync::atomic::AtomicU64;
 use std::io::{BufWriter, Write};
 
 use futures::future;
@@ -26,13 +25,6 @@ mod block_index;
 mod searcher;
 mod ranker;
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let crawler = Crawler::new();
-//     crawler.run("https://en.wikipedia.org/wiki/Main_Page");
-//     Ok(())
-// }
-
 fn visit_dirs<F: FnMut(DirEntry)>(dir: &Path, cb: &mut F) -> std::io::Result<()> {
     if dir.is_dir() {
         for entry in std::fs::read_dir(dir)? {
@@ -48,70 +40,77 @@ fn visit_dirs<F: FnMut(DirEntry)>(dir: &Path, cb: &mut F) -> std::io::Result<()>
     Ok(())
 }
 
-// fn main() {
-//     let parser = Parser::new();
-//     let mut indexer = Indexer::new(
-//         Box::new(BlockIndex::new(BlockIndexConfig::default())),
-//         Box::new(DocumentFileStorage::new("index/document_storage"))
-//     );
-//     let searcher = Searcher::new();
-//
-//     let t0 = std::time::Instant::now();
-//     let mut document_index = 0;
-//     visit_dirs(
-//         "data/wiki".as_ref(),
-//         &mut |entry| {
-//             let content = std::fs::read_to_string(entry.path()).unwrap();
-//             let parsed_content = parser.parse(&content);
-//             indexer.add_document(Document::new(
-//                 parsed_content.title.unwrap_or(String::new()),
-//                 parsed_content.tokens
-//             ));
-//             document_index += 1;
-//         }
-//     ).unwrap();
-//     println!("Built index in: {} seconds", (std::time::Instant::now() - t0).as_millis() as f64 / 1000.0);
-//
-//     indexer.print_stats();
-//
-//     let results = searcher.intersection_search_documents(&indexer, vec!["computer".to_owned(), "hardware".to_owned()]);
-//     println!("Search results: {}", results.len());
-// }
+async fn main_crawl() -> Result<(), Box<dyn std::error::Error>> {
+    let mut crawler = Crawler::new();
 
-// async fn load_and_parse(parser: &Parser, path: PathBuf) -> Tokens {
-//     let content = tokio::fs::read_to_string(path).await.unwrap();
-//     parser.parse(&content)
-// }
-//
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let parser = Parser::new();
-//     let mut indexer = Indexer::new();
-//     let t0 = std::time::Instant::now();
-//
-//     let mut files = Vec::new();
-//     visit_dirs(
-//         "data/wiki".as_ref(),
-//         &mut |entry| {
-//             files.push(entry);
-//         }
-//     ).unwrap();
-//
-//     let mut load_and_parse_tasks = Vec::new();
-//     for path in files {
-//         load_and_parse_tasks.push(load_and_parse(&parser, path.path()));
-//     }
-//
-//     for tokens in future::join_all(load_and_parse_tasks).await {
-//         indexer.add_document(Document::new(tokens));
-//     }
-//     println!("Built index in: {} seconds", (std::time::Instant::now() - t0).as_millis() as f64 / 1000.0);
-//
-//     indexer.print_stats();
-//     Ok(())
-// }
+    let result_receiver = crawler.result_receiver.clone();
+    // std::thread::spawn(move || {
+    //     for result in &result_receiver {
+    //         // Save content to disk
+    //         let (save_path_folder, save_path_name) = crawler::get_save_path(&result.url);
+    //         let base_path = format!("data/{}", save_path_folder);
+    //         std::fs::create_dir_all(&base_path);
+    //         std::fs::write(format!("{}/{}", base_path, save_path_name), result.content);
+    //     }
+    // });
+    let handle = std::thread::spawn(move || {
+        let parser = Parser::new();
+        let mut indexer = Indexer::new(
+            Box::new(BlockIndex::new(BlockIndexConfig::default())),
+            Box::new(DocumentFileStorage::new("index/document_storage"))
+        );
 
-fn main() {
+        for result in &result_receiver {
+            if let Some(result) = result {
+                let parsed_content = parser.parse(&result.content);
+                indexer.add_document(Document::new(
+                    parsed_content.title.unwrap_or(String::new()),
+                    parsed_content.tokens
+                ));
+            } else {
+                break;
+            }
+        }
+
+        println!("Exited.");
+    });
+
+    crawler.run("https://en.wikipedia.org/wiki/Main_Page");
+    handle.join();
+    Ok(())
+}
+
+fn main_index_from_files() {
+    let parser = Parser::new();
+    let mut indexer = Indexer::new(
+        Box::new(BlockIndex::new(BlockIndexConfig::default())),
+        Box::new(DocumentFileStorage::new("index/document_storage"))
+    );
+    let searcher = Searcher::new();
+
+    let t0 = std::time::Instant::now();
+    let mut document_index = 0;
+    visit_dirs(
+        "test_data/wiki".as_ref(),
+        &mut |entry| {
+            let content = std::fs::read_to_string(entry.path()).unwrap();
+            let parsed_content = parser.parse(&content);
+            indexer.add_document(Document::new(
+                parsed_content.title.unwrap_or(String::new()),
+                parsed_content.tokens
+            ));
+            document_index += 1;
+        }
+    ).unwrap();
+    println!("Built index in: {} seconds", (std::time::Instant::now() - t0).as_millis() as f64 / 1000.0);
+
+    indexer.print_stats();
+
+    let results = searcher.intersection_search_documents(&indexer, vec!["computer".to_owned(), "hardware".to_owned()]);
+    println!("Search results: {}", results.len());
+}
+
+fn main_search() {
     let mut indexer = Indexer::new(
         Box::new(BlockIndex::new(BlockIndexConfig::use_existing())),
         Box::new(DocumentFileStorage::from_existing("index/document_storage"))
@@ -137,4 +136,11 @@ fn main() {
     for (document_id, score) in results.iter().take(20) {
         println!("{} (id: {}): ({})", indexer.document_storage().get_title(*document_id), document_id, score);
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    main_crawl().await.unwrap();
+    // main_search();
+    Ok(())
 }
